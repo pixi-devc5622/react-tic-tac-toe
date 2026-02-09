0f870941ac9014fd1bf8a544d488c7993000f410
pixi-devc5622 <dev@pixiverse.in>
2026-02-09 12:00:26 +0530

    Updating Game Title

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className=" flex rounded-xl items-center justify-center bg-gray-700">
       <div className=" p-8 rounded-xl shadow-lg w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center mb-6">Tic Tac Toe</h1>
+        <h1 className="text-3xl font-bold text-center mb-6">
+          Project - Tic Tac Toe
+        </h1>
 
         <Board />
       </div>
